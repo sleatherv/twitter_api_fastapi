c33341bbdd5046401410653c08737bf30a590652
sleatherv <sleatherv28@hotmail.com>
2022-04-27 20:04:05 -0500

    [ADD] User, UserBase and UserLogin model

--- models/user.py
+++ models/user.py
@@ -1,6 +1,34 @@
+#Pyton
+from uuid import UUID
+from typing import Optional
+
 #Pydantic
 from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
 
-class User(BaseModel):
-    pass
+
+class UserBase(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+
+class User(UserBase):
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    birth_date: Optional[date] = Field(default=None)
+
+
+class UserLogin(UserBase):
+    password: str = Field(
+        ...,
+        min_length=8,
+    )
 
