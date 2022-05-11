24271350bab971c4363e7fd35e4ddf961a2dfcce
sleatherv <sleatherv28@hotmail.com>
2022-05-10 21:57:42 -0500

    show_all_users

--- main.py
+++ main.py
@@ -62,14 +62,29 @@ def login():
 
 
 @app.get(
-    path='/user',
+    path='/users',
     response_model=List[User],
     status_code=status.HTTP_200_OK,
     summary="Show all users",
     tags=["Users"]
 )
 def show_all_users():
-    pass
+    """
+    This path operation shows all user in the app
+
+    Parameters: None
+        
+    
+    Return a json with all users in the app, with the following:
+    - user_id: UUID
+    - email: Emailstr
+    - first_name: str
+    - last_name: str
+    - birth_date: date
+    """
+    with open("users.json", "r", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        return results
 
 # Show a user
 
