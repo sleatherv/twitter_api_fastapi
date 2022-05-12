0f5280cdb6f2d09f162c0c21e352e14516296190
sleatherv <sleatherv28@hotmail.com>
2022-05-11 22:04:37 -0500

    post a tweet

--- main.py
+++ main.py
@@ -147,8 +147,37 @@ def home():
     summary="Post a tweet",
     tags=["Tweets"]
 )
-def post():
-    pass
+def post(tweet: Tweet = Body(...)):
+    """
+    Post a tweet
+
+    This path operation post a tweet in the app
+
+    Parameters: None
+    - Request body parameter
+        -tweet: Tweet
+    
+    Return a json with the tweet information:
+    - tweet_id: UUID 
+    - content: str
+    - created_at: datetime 
+    - updated_at: Optional[datetime]
+    - by: User
+    """
+    with open("tweets.json", "r+", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        tweet_dict = tweet.dict()
+        tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"])
+        tweet_dict["created_at"] = str(tweet_dict["created_at"])
+        # if tweet_dict["updated_at"]:
+        tweet_dict["updated_at"] = str(tweet_dict["updated_at"])
+        tweet_dict["by"]["user_id"] = str(tweet_dict["by"]["user_id"])
+        tweet_dict["by"]["birth_date"] = str(tweet_dict["by"]["birth_date"])
+
+        results.append(tweet_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+        return tweet
 
 
 ### Show a tweet
